1656f51f915389cda6cec502975543e46651d8e1
Admin <starikford@gmail.com>
2023-11-05 19:33:58 +0300

    domashka here

---
+++ task1.py
@@ -0,0 +1,52 @@
+print("Task1")
+print(17/2*3+2)
+print(2+17/2*3)
+print(19%4+15/2*3)
+print(15+6-10*4)
+print(17/2%2*3**3)
+print()
+"""start task2"""
+print("Task 2")
+print(17/2*(3+2))
+print((2+17)/2*3)
+print(19%(4+15)/2*3)
+print((15+6-10)*4)
+print(17/2%(2*3)**3)
+"""start task3"""
+print("")
+print("Task3")
+money_anna= 11
+costs_bread= 1.50
+vivod= money_anna/costs_bread
+print(format(vivod, '.2f'),'рублей')
+print()
+"""start task 3"""
+print("Task 4")
+apple_Anna= 2
+apple_Pol= 5
+print("apple Anna=",apple_Anna)
+print("apple Pol=",apple_Pol)
+print()
+"""print task 5"""
+print("start task 5")
+days = 5
+print(days,"суток=",days*24,"часов=",days*1440,"минут=",days*1440*60,"секунд")
+print()
+"""start task 6"""
+print("task 6")
+print(182//7,"недель")
+print()
+"""start task 7"""
+print("task 7")
+side_1= 150  #сторона прямоугольника a
+side_2= 65 # сторона прямоугольника b
+side_kvadrat= 30
+print("можно отрезать",side_1*side_2//side_kvadrat**2,"квадратов")
+print()
+"""start task 8"""
+print("start task 8")
+seconds= 4000
+print(seconds//60,"минут")
+print(seconds//3600, "часов")
+print()
+print('game set and match')
