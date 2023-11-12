9bf1540895022e49ce59b6662ef89e890e7ca96a
Admin <starikford@gmail.com>
2023-11-12 19:52:30 +0300

    task 3 ,4 is done

--- hard_task.py
+++ hard_task.py
@@ -1,15 +1,16 @@
+"""task 3"""
 some_string = '192.168.0.1'
 some_string2 = some_string.replace('.','')
 some_string3=  int(some_string2[:4:])
 some_string4 = int(some_string2[4::])
 print()
 
-"task 4"
+"""task 4"""
 print(some_string3 + some_string4)
 print()
 some_word = ('predecessore')
 print(some_word.count('e'))
 print()
 
-"task 5"
+
 
